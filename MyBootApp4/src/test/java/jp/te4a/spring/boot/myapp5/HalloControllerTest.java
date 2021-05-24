421e65c4c5f1b96e6ff100cc9cb7a9c3914daa3e
岩崎 拓海 <180046@jc-21.jp>
2021-05-24 16:21:20 +0900

    バージョンによっては使えない

---
+++ MyBootApp4/src/test/java/jp/te4a/spring/boot/myapp5/HalloControllerTest.java
@@ -0,0 +1,10 @@
+package jp.te4a.spring.boot.myapp5;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class HalloControllerTest {
+	
+	@Test
+	void 正常系_トップページ表示() {
+	}
+
+}
